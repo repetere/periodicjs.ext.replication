29e95fa9f965f48e9a085492b12515ea923c0503
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-10-31 18:16:42 -0400

    working replication

--- controller/replication.js
+++ controller/replication.js
@@ -13,74 +13,108 @@ var Utilities = require('periodicjs.core.utilities'),
 	appSettings,
 	mongoose,
 	logger,
+	restoreController,
 	replicationconffilepath,
 	replicationSettings,
+	replicationSSHConnection,
 	uploadreplicationdir = path.resolve(process.cwd(), 'content/files/replications'),
+	remotefiletodownload,
+	localfiletosave = path.resolve(process.cwd(), 'content/files/backups/replicationsnapshot.zip'),
 	d = new Date(),
 	defaultExportFileName = 'dbemptyreplication' + '-' + d.getUTCFullYear() + '-' + d.getUTCMonth() + '-' + d.getUTCDate() + '-' + d.getTime() + '.json';
 
+
 /**
- * get the replication settings
+ * restore the downloaded zip file and remove it when done
+ * @param  {Function} asyncCallBack
+ * @return {Function} async callback asyncCallBack(err,results);
+ */
+var restoreFromReplicationBackup = function (asyncCallBack) {
+	restoreController.restoreBackup({
+		file: localfiletosave,
+		removebackup: true
+	}, asyncCallBack);
+};
+
+/**
+ * get the replication backup file zip file
  * @param  {Function} asyncCallBack
  * @return {Function} async callback asyncCallBack(err,results);
  */
 var getReplicationData = function (replicationSettings, asyncCallBack) {
 	try {
 		var conn = new Connection();
-		var onShell1 = function () {
-			conn.exec('cd /var/www/promise-web-application/current/periodicjs && ls && node index.js --cli --extension dbseed --task export', function (err, stream) {
-				stream.on('exit', function (code, signal) {
-					console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
-					asyncCallBack(null, 'Stream :: exit :: code: ' + code + ', signal: ' + signal);
-				});
-				stream.on('close', function () {
-					console.log('Stream :: close');
-					// conn.shell('ls');
-					conn.end();
-
-				});
-				stream.on('data', function (data) {
-					console.log('STDOUT: ' + data);
-				});
-				stream.stderr.on('data', function (data) {
-					console.log('STDERR: ' + data);
-				});
-				stream.on('end', function () {
-					conn.end();
-				});
+		var downloadProgressStatus = function (downloaded, chunk, total) {
+			console.log('downloaded', Math.floor((downloaded / total) * 100), '%', 'total', total, 'bytes');
+		};
+		var downloadReplicationBackup = function () {
+			conn.sftp(function (err, sftp) {
+				if (err) {
+					asyncCallBack(err);
+				}
+				else {
+					fs.ensureDirSync(path.resolve(process.cwd(), 'content/files/backups'));
+					remotefiletodownload = path.join(replicationSettings.webAppPath, 'content/files/backups/replicationsnapshot.zip');
+					sftp.fastGet(remotefiletodownload, localfiletosave, {
+							step: downloadProgressStatus
+						},
+						function (err) {
+							if (err) {
+								asyncCallBack(err);
+							}
+							else {
+								console.log('stfp :: exit ::');
+								asyncCallBack(null);
+								conn.end();
+							}
+						});
+				}
 			});
-			// conn.shell('ls -l', function (err, stream) {
-			// 	if (err) {
-			// 		asyncCallBack(err);
-			// 	}
-			// 	stream.on('close', function () {
-			// 		console.log('Stream :: close');
-			// 		conn.end();
-			// 	});
-			// 	stream.on('data', function (data) {
-			// 		console.log('STDOUT: ' + data);
-			// 	});
-			// 	stream.stderr.on('data', function (data) {
-			// 		console.log('STDERR: ' + data);
-			// 	});
-			// 	stream.end('cd /var/www/promise-web-application/current/periodicjs && ls');
+		};
+		var onShell1 = function () {
+			conn.exec('cd ' + replicationSettings.webAppPath + ' && ls && node index.js --cli --extension backup --task backup --filename replicationsnapshot', function (err, stream) {
+				if (err) {
+					asyncCallBack(err);
+				}
+				else {
+					stream.on('exit', function (code, signal) {
+						console.log('Stream :: exit :: code: ' + code + ', signal: ' + signal);
+						downloadReplicationBackup();
+						// asyncCallBack(null, 'Stream :: exit :: code: ' + code + ', signal: ' + signal);
+					});
+					stream.on('close', function () {
+						console.log('Stream :: close');
+					});
+					stream.on('data', function (data) {
+						console.log('STDOUT: ' + data);
+					});
+					stream.stderr.on('data', function (data) {
+						console.log('STDERR: ' + data);
+					});
+					stream.on('end', function () {
+						console.log('Stream :: end');
+					});
+					stream.on('error', function (err) {
+						asyncCallBack(err);
+					});
+				}
+			});
+		};
+		conn.on('ready', function () {
+			console.log('Connection :: readys');
+			conn.shell(onShell1);
 		});
-};
-conn.on('ready', function () {
-	console.log('Connection :: readys');
-	conn.shell(onShell1);
-});
-conn.connect(replicationSettings);
-conn.on('error', function (err) {
-	asyncCallBack(err);
-});
-conn.on('end', function () {
-	asyncCallBack(null, 'got replication');
-});
-}
-catch (e) {
-	asyncCallBack(e);
-}
+		conn.connect(replicationSettings);
+		conn.on('error', function (err) {
+			asyncCallBack(err);
+		});
+		conn.on('end', function () {
+			asyncCallBack(null, 'got replication');
+		});
+	}
+	catch (e) {
+		asyncCallBack(e);
+	}
 };
 
 /**
@@ -121,7 +155,8 @@ var replicate_periodic = function (options, asyncCallBack) {
 			cb(null, environment);
 		},
 		getReplicationConfig,
-		getReplicationData
+		getReplicationData,
+		restoreFromReplicationBackup
 	], function (err, results) {
 		asyncCallBack(err, results);
 	});
@@ -212,7 +247,7 @@ var controller = function (resources) {
 	replicationconffilepath = path.join(CoreExtension.getconfigdir({
 		extname: 'periodicjs.ext.replication'
 	}), 'settings.json');
-
+	restoreController = require('../../periodicjs.ext.backup/controller/restorebackup')(resources);
 	return {
 		index: index,
 		replicate_periodic: replicate_periodic,
